7ec2760a538e8f7d916a18c1ea02b6768f11b039
Niraj Dev <devniraj4@gmail.com>
2026-07-13 21:35:32 +0545

    Add a New File README.txt

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -9,20 +9,23 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ksr2k4^p4s7)76203rc_)##aflh)%v@nanb#3cq)fb@++)5v@5'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+load_dotenv(BASE_DIR / '.env') 
+
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
